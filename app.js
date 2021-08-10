11d0ee375c8773ef2c5b55d905031e995ed01512
Frank South <frankstonesouth@gmail.com>
2021-08-10 16:43:54 -0400

    Added more functionality to etch-a-sketch

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const container = document.querySelector('.container');
-
+const button = document.querySelector('.clear');
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (let i = 0; i < rows * cols; i++) {
     let newDiv = document.createElement('div');
-    newDiv.innerText = i + 1;
+    // newDiv.innerText = i + 1;
     container.appendChild(newDiv).className = 'grid-item';
   }
 }
 
 makeRows(16, 16);
+
+const childItem = document.querySelectorAll('.grid-item');
+for (let child of childItem) {
+  child.addEventListener('mouseover', function () {
+    child.classList.add('color');
+  });
+}
